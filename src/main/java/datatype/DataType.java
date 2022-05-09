7f2259aaf807ad072fbf14dfb936cdca514a1d87
Deamacto <pszon.joanna@gmail.com>
2022-05-09 14:41:24 +0200

    changed paths to file

--- src/main/java/datatype/DataType.java
+++ src/main/java/datatype/DataType.java
@@ -7,22 +7,22 @@ public class DataType {
 
     public DataType(Game game) {
         if(game == Game.BINARY6x6) {
-            file = "binary_6x6";
+            file = "src/data/binary_6x6";
             size = 6;
         } else if(game == Game.BINARY8x8) {
-            file = "binary_8x8";
+            file = "src/data/binary_8x8";
             size = 8;
         } else if(game == Game.BINARY10x10) {
-            file = "binary_10x10";
+            file = "src/data/binary_10x10";
             size = 10;
         } else if(game == Game.FUTOSHIKI4x4) {
-            file = "futoshiki_4x4";
+            file = "src/data/futoshiki_4x4";
             size = 7;
         } else if(game == Game.FUTOSHIKI5x5) {
-            file = "futoshiki_5x5";
+            file = "src/data/futoshiki_5x5";
             size = 9;
         } else if(game == Game.FUTOSHIKI6x6) {
-            file = "futoshiki_6x6";
+            file = "src/data/futoshiki_6x6";
             size = 11;
         }
     }
