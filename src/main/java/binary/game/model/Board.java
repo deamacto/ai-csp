9866e7be73d6f89e41dd3c2612fdd7b6c1af7e96
Deamacto <pszon.joanna@gmail.com>
2022-05-09 22:38:41 +0200

    added method

--- src/main/java/binary/game/model/Board.java
+++ src/main/java/binary/game/model/Board.java
@@ -1,12 +1,48 @@
 package binary.game.model;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Board {
     private Integer[][] board;
 
     public Board(Integer[][] board) {
         this.board = board;
+    }
+
+    public boolean checkSameAmountOnWholeBoard() {
+        ArrayList<Integer> zeros = new ArrayList<>();
+        ArrayList<Integer> ones = new ArrayList<>();
+
+        for(int i = 0; i < board.length; i++) {
+            zeros.add(0);
+            ones.add(0);
+        }
+
+        int zero = 0;
+        int one = 0;
+
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(board[i][j] == 1) {
+                    one += 1;
+                    ones.set(j, ones.get(j) + 1);
+                } else if(board[i][j] == 0) {
+                    zero += 1;
+                    zeros.set(j, zeros.get(j) +1);
+                }
+            }
+            if(zero != one) {
+                return false;
+            }
+        }
+
+        for(int i = 0; i < zeros.size(); i++) {
+            if(!zeros.get(i).equals(ones.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
