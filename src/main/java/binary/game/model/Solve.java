bcc9a032cdb909ce4062a6e711f7277ee3e12e0b
Deamacto <pszon.joanna@gmail.com>
2022-05-27 23:59:24 +0200

    added backtracking method

--- src/main/java/binary/game/model/Solve.java
+++ src/main/java/binary/game/model/Solve.java
@@ -1,9 +1,31 @@
 package binary.game.model;
 
+import util.Coordinates;
+
+import java.util.ArrayList;
+
 public class Solve {
 
-    public void backtracking(Board emptyBoard) {
-        Board solution = emptyBoard.clone();
+    public static ArrayList<Board> backtracking(Board emptyBoard) {
+        ArrayList<Board> solutions = new ArrayList<>();
+        Board currentBoard = emptyBoard.clone();
+        return backtrack(currentBoard, solutions);
+    }
 
+    private static ArrayList<Board> backtrack(Board currentBoard, ArrayList<Board> solutions) {
+        Coordinates spot = currentBoard.findFirstEmptySpot();
+
+        if(spot.x == -1 && spot.y == -1) {
+            solutions.add(currentBoard);
+        } else {
+            for(int domainElement : Board.domain) {
+                Board newBoard = currentBoard.clone();
+                if(newBoard.setTileIfPossible(spot, domainElement)) {
+                    solutions = backtrack(newBoard, solutions);
+                }
+            }
+        }
+
+        return solutions;
     }
 }
