8dbc77256bb9ce3831054322d4db9602fecee9ef
Deamacto <pszon.joanna@gmail.com>
2022-05-29 23:15:11 +0200

    added spaghetti toString method

--- src/main/java/futoshiki/game/model/Board.java
+++ src/main/java/futoshiki/game/model/Board.java
@@ -8,4 +8,31 @@ public class Board {
         this.board = board;
         this.domain = domain;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder f = new StringBuilder("Futoshiki \n");
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if (board[i][j].number == null) {
+                    f.append(" _ ");
+                } else {
+                    f.append(" ").append(board[i][j].number).append(" ");
+                }
+
+                if (board[i][j].rightRelation != Symbol.NONE) {
+                    f.append(board[i][j].rightRelation.getSymbol());
+                }
+            }
+            f.append("\n");
+
+            for(int k = 0; k < board[0].length; k++) {
+                if (board[i][k].bottomRelation != Symbol.NONE) {
+                    f.append(" ").append(board[i][k].bottomRelation.getSymbol()).append("  ");
+                }
+            }
+            f.append("\n");
+        }
+        return f.toString();
+    }
 }
