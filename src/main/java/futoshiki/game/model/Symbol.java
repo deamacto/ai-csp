9312730d63b08cae69d6a2084dbd9b999dca6e09
Deamacto <pszon.joanna@gmail.com>
2022-05-29 23:14:34 +0200

    changed enum to have string values

--- src/main/java/futoshiki/game/model/Symbol.java
+++ src/main/java/futoshiki/game/model/Symbol.java
@@ -1,8 +1,18 @@
 package futoshiki.game.model;
 
 public enum Symbol {
-    PLACEHOLDER,
-    GREATER,
-    LESSTHAN,
-    NONE;
+    PLACEHOLDER(" "),
+    GREATER(">"),
+    LESSTHAN("<"),
+    NONE("");
+
+    private String symbol;
+
+    private Symbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
