0b498de59a26452a583529b4dba515a8878c4ffc
Asia <pszon.joanna@gmail.com>
2022-05-31 13:14:38 +0200

    changed Field to have domain

--- src/main/java/futoshiki/game/model/Field.java
+++ src/main/java/futoshiki/game/model/Field.java
@@ -1,18 +1,22 @@
 package futoshiki.game.model;
 
+import java.util.ArrayList;
+
 public class Field {
     public Symbol leftRelation;
     public Symbol rightRelation;
     public Symbol topRelation;
     public Symbol bottomRelation;
     public Integer number;
+    public ArrayList<Integer> domain;
 
-    public Field(Symbol leftRelation, Symbol rightRelation, Symbol topRelation, Symbol bottomRelation, Integer number) {
+    public Field(Symbol leftRelation, Symbol rightRelation, Symbol topRelation, Symbol bottomRelation, Integer number, ArrayList<Integer> domain) {
         this.leftRelation = leftRelation;
         this.rightRelation = rightRelation;
         this. topRelation = topRelation;
         this.bottomRelation = bottomRelation;
         this.number = number;
+        this.domain = domain;
     }
 
     public void normalize() {
