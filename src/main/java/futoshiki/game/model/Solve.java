d584bf06243c01bf34782f0c017cf3317f99aef4
Asia <pszon.joanna@gmail.com>
2022-05-31 15:07:09 +0200

    updated Solve to have counter

--- src/main/java/futoshiki/game/model/Solve.java
+++ src/main/java/futoshiki/game/model/Solve.java
@@ -4,13 +4,18 @@ import util.Coordinates;
 import java.util.ArrayList;
 
 public class Solve {
+
+    public static int counter = 0;
+
     public static ArrayList<Board> backtracking(Board emptyBoard) {
+        counter = 0;
         ArrayList<Board> solutions = new ArrayList<>();
         Board currentBoard = emptyBoard.clone();
         return backtrack(currentBoard, solutions);
     }
 
     private static ArrayList<Board> backtrack(Board currentBoard, ArrayList<Board> solutions) {
+        counter++;
         Coordinates spot = currentBoard.findFirstEmptySpot();
 
         if(spot.x == -1 && spot.y == -1) {
@@ -28,18 +33,19 @@ public class Solve {
     }
 
     public static ArrayList<Board> forwardchecking(Board startingBoard) {
+        counter = 0;
         ArrayList<Board> solutions = new ArrayList<>();
         Board currentBoard = startingBoard.clone();
         return forwardcheck(currentBoard, solutions);
     }
 
     private static ArrayList<Board> forwardcheck(Board currentBoard, ArrayList<Board> solutions) {
+        counter++;
         Coordinates spot = currentBoard.findFirstEmptySpot();
 
         if(spot.x == -1 && spot.y == -1) {
             solutions.add(currentBoard);
         } else {
-            var foobar = currentBoard.getTile(spot).domain;
             for(Integer domainElement : currentBoard.getTile(spot).domain) {
                 Board newBoard = currentBoard.clone();
                 if(newBoard.setFieldIfPossible(spot, domainElement) && newBoard.deleteFromDomain(spot)) {
